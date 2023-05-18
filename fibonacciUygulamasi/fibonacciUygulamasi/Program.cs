d94a06984401aa78d73982018eb7cc115a6099ea
Teoman BAŞ <teomanbas@protonmail.com>
2023-05-18 00:18:35 -0400

    Yeni Ornek Eklendi.

---
+++ fibonacciUygulamasi/fibonacciUygulamasi/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] fib = new int[10];
+        fib[0] = 0;
+        fib[1] = 1; 
+        for(int i=2; i<fib.Length; i++)
+        {
+            fib[i] = fib[i-1] + fib[i-2];
+            
+        }
+        foreach (int i in fib)
+        {
+            Console.Write(i+" ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("------------------------");
+
+
+        Console.WriteLine(fibo(5));
+
+        Console.ReadKey();
+    }
+    static int fibo(int x)
+    {
+        if (x <= 0 || x==1) { return 1; }
+        else
+        {
+            return fibo(x - 1) + fibo(x - 2);
+        }
+    }
+
+
+}
